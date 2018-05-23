9a0290411b9f7aecab1f935d71e2242c03676a85
Bakkies <elton.bucky@gmail.com>
2018-05-23 08:08:34 +0200

    Continue Backend Design

--- resources/custom/pages/movie.blade.php
+++ resources/custom/pages/movie.blade.php
@@ -10,7 +10,21 @@
 <img src="{{ url( 'img/movies/' . $movie->image ) }}" class="img-responsive">
 <p>{{ $movie->description }}</p>
 
-<embed width="420" height="315"
-src="{{ $movie->trailerLink }}">
-<iframe width="853" height="480" src="{{ $movie->trailerLink }}" frameborder="0" allowfullscreen ng-show="showvideo"></iframe>
+@if( !empty( $movie->trailerLink ) )
+
+  @if( strpos( $movie->trailerLink, 'embed' ) !== false )
+    <div class="col-sm-6 col-xs-12 mg-b30 move-bf">
+     <div class="youtube">
+       <iframe width="560" height="315" src="{{ $movie->trailerLink }}" frameborder="0" allowfullscreen></iframe>
+     </div>
+    </div>
+  @else( strpos( $movie->trailerLink, 'watch' ) !== false )
+    <div class="col-sm-6 col-xs-12 mg-b30 move-bf">
+     <div class="youtube">
+       <iframe width="560" height="315" src="https://www.youtube.com/embed/dxWvtMOGAhw?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+     </div>
+    </div>
+  @endif
+
+@endif
 @endsection
