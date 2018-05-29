c41c453117849a187654fd0d6e58af683280fea3
Bakkies <elton.bucky@gmail.com>
2018-05-29 09:50:09 +0200

    Added Flashy

--- app/TopTenSeries.php
+++ app/TopTenSeries.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Relations\Pivot;
 
-class TopTenMovie extends Pivot
+class TopTenSeries extends Pivot
 {
 	use \Rutorika\Sortable\SortableTrait;
 
--- config/app.php
+++ config/app.php
@@ -176,8 +176,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-		
-		App\Providers\UploadServiceProvider::class,
+		    App\Providers\ConfigServiceProvider::class,
+		    App\Providers\UploadServiceProvider::class,
+        MercurySeries\Flashy\FlashyServiceProvider::class
 
     ],
 
@@ -227,7 +228,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Flashy' => MercurySeries\Flashy\Flashy::class,
     ],
 
 ];
